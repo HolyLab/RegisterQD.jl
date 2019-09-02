6d3b697c89ecafd49de09f6f75b0d016f665f786
Tim Holy <tim.holy@gmail.com>
2019-09-02 07:22:28 -0500

    Rework qd_rigid to always return a rigid transformation
    This is a major breaking change

--- src/RegisterQD.jl
+++ src/RegisterQD.jl
@@ -16,8 +16,20 @@ include("gridsearch.jl")
 
 export qd_translate,
         qd_rigid,
-        qd_affine
+        qd_affine,
+        arrayscale,
         grid_rotations,
         rotation_gridsearch
+
+# Deprecations
+function qd_rigid(fixed, moving, mxshift, mxrot, minwidth_rot, SD=I; kwargs...)
+    error("""
+    `qd_rigid` has fundamentally changed. For the new syntax, see the help (`?qd_rigid`).
+    In particular, note that the transformation is now returned in *physical*
+    units rather than *array-index* units---if you were using something different
+    from the identity matrix for `SD`, this is a change in behavior.
+    As a consequence, your old results may not be comparable with your new results.
+    """)
+end
 
 end # module
